d40891e0e9cf7df43186a60a45ae2cbe39631d94
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-06-26 16:56:54 +0200

    json output format ok

--- init.jl
+++ init.jl
@@ -1,4 +1,5 @@
-using JuMP
-using GLPK
-using JSON
-using Sockets
+using JuMP      #ilp/mip model
+using GLPK      #optimizer
+using JSON      #serialization and parsing
+using Sockets   #tcp server
+using Dates     #timestamp
