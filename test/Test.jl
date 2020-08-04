83c3a8ca21f6f58ef3b07fa86761d1a0bd362764
Tommy Anfosso <anfossotommy@hotmail.fr>
2020-08-04 19:44:02 +0200

    gen a lair ok

--- test/Test.jl
+++ test/Test.jl
@@ -6,6 +6,11 @@ module Test
     using Dates
     using Random
 
+    mutable struct Coord
+        x::Int32
+        y::Int32
+    end
+
     # mutable struct Test
     #     input::Pdp.Input
     #     output::Pdp.Output
@@ -24,9 +29,9 @@ module Test
 
     function performTesting()
         INSTANCES = 
-                [RandomInstance(5, 1, 4)
-                ,RandomInstance(20, 4, 16)
-                ,RandomInstance(80, 16, 64)]
+                [RandomInstance(5, 1, 4)        #Nmin = 6   Nmax = 9
+                ,RandomInstance(20, 4, 16)      #Nmin = 24  Nmax = 36
+                ,RandomInstance(80, 16, 64)]    #Nmin = 96  Nmax = 144
 
         # for instance in INSTANCES
         instance = "onsenfout"
@@ -70,38 +75,44 @@ module Test
 
     function genInput(instance)
         MAX_DIST = 1000
-        MAX_TIME = 1679 #3h59 le lendemain
+        PLAN_SIZE = MAX_DIST / sqrt(2)
+        TIME_CYCLE = 1679 #3h59 le lendemain
         MIN_WAITING_TIME = 5
         MAX_WAITING_TIME = 30
-        MIN_RIDE_TIME = 5
         MAX_RIDE_TIME = 180 #3 hours
         INF = 99999
         m = rand(instance.minPerClass:instance.maxPerClass)
         n = instance.population - m
         N = 2*n + m
-        c = Array{Int32,2}(undef, N, N)
+        
+        # generer N paires d'entiers générés dans le plan
+        rands = rand(0:PLAN_SIZE, (2*N))
+        coords = Array{Coord, 1}(undef, N)
         for i in 1:N
-            c[i,i] = INF
+            coords[i] = Coord(rands[i], rands[i+N])
+        end
+
+        # calculer les distances entre chaque sommet et temps necessaire
+        c = fill(INF, (N,N))
+        t = fill(INF, (N,N))
+        for i in 1:N
             for j in i+1:N
-                c[i,j] = rand(0:MAX_DIST)
-                c[j,i] = c[i,j]
+                c[i,j] = c[j,i] = Int(round(sqrt((coords[j].x-coords[i].x)^2+(coords[j].y-coords[i].y)^2)))
+                t[i,j] = t[j,i] = Int(round(c[i,j] * MAX_RIDE_TIME / MAX_DIST))
             end
         end
-        t = c
-        e = Array{Int32}(undef, N)
-        l = Array{Int32}(undef, N)
-        for i in 1:m
-            e[i] = 0
-            l[i] = INF
-        end
+
+        # calculer les temps au plus tot et au plus tard
+        e = fill(0, N)
+        l = fill(INF, N)
         for i in m+1:m+n
-            e[i] = rand(0:MAX_TIME)
+            e[i] = rand(0:TIME_CYCLE)
             l[i] = e[i] + rand(MIN_WAITING_TIME:MAX_WAITING_TIME)
         end
         for i in m+n+1:N
-            e[i] = 0
-            l[i] = e[i-n] + rand(MIN_RIDE_TIME:MAX_RIDE_TIME)
+            l[i] = l[i-n] + rand(t[i-n,i]:t[i-n,i]+MAX_WAITING_TIME)
         end
+
         # println(m)
         # println(n)
         # println(c)
